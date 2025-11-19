204d5348ed21e49d48c07ee5d36d1e5d4a90c44d
João Pedro <mielsen.gamer2005@gmail.com>
2025-11-19 09:25:52 -0300

    Docs: Add XML comments to ParameterDefinition and IMethodBaseBuilderExtensions

---
+++ Meta/Utils.Metalama.Extensions/Definitions/ParameterDefinition.cs
@@ -0,0 +1,71 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code;
+
+namespace LightningArc.Utils.Metalama;
+
+/// <summary>
+/// Represents the definition of a method parameter at compile time.
+/// </summary>
+[CompileTime]
+public class ParameterDefinition
+{
+    /// <summary>
+    /// Gets or sets the name of the parameter.
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the type of the parameter.
+    /// </summary>
+    public required IType Type { get; set; }
+
+    /// <summary>
+    /// Gets or sets the reference kind of the parameter (e.g., in, out, ref). Defaults to <see cref="RefKind.None"/>.
+    /// </summary>
+    public RefKind RefKind { get; set; } = RefKind.None;
+
+    /// <summary>
+    /// Gets or sets the default value of the parameter. Defaults to <see langword="null"/>.
+    /// </summary>
+    public TypedConstant? DefaultValue { get; set; } = null;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ParameterDefinition"/> class with an <see cref="IType"/>.
+    /// </summary>
+    /// <param name="name">The name of the parameter.</param>
+    /// <param name="type">The type of the parameter as an <see cref="IType"/>.</param>
+    /// <param name="refKind">The reference kind of the parameter. Defaults to <see cref="RefKind.None"/>.</param>
+    /// <param name="defaultValue">The default value of the parameter. Defaults to <see langword="null"/>.</param>
+    public ParameterDefinition(
+        string name,
+        IType type,
+        RefKind refKind = RefKind.None,
+        TypedConstant? defaultValue = null
+    )
+    {
+        Name = name;
+        Type = type;
+        RefKind = refKind;
+        DefaultValue = defaultValue;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ParameterDefinition"/> class with a <see cref="Type"/>.
+    /// </summary>
+    /// <param name="name">The name of the parameter.</param>
+    /// <param name="type">The type of the parameter as a <see cref="Type"/>.</param>
+    /// <param name="refKind">The reference kind of the parameter. Defaults to <see cref="RefKind.None"/>.</param>
+    /// <param name="defaultValue">The default value of the parameter. Defaults to <see langword="null"/>.</param>
+    public ParameterDefinition(
+        string name,
+        Type type,
+        RefKind refKind = RefKind.None,
+        TypedConstant? defaultValue = null
+    )
+    {
+        Name = name;
+        Type = TypeFactory.GetType(type);
+        RefKind = refKind;
+        DefaultValue = defaultValue;
+    }
+}
---
+++ Meta/Utils.Metalama.Extensions/Extensions/IMethodBaseBuilderExtensions.cs
@@ -0,0 +1,54 @@
+﻿using Metalama.Framework.Aspects;
+using Metalama.Framework.Code.DeclarationBuilders;
+using System.Collections.Generic;
+
+namespace LightningArc.Utils.Metalama;
+
+/// <summary>
+/// Provides extension methods for <see cref="IMethodBaseBuilder"/>.
+/// </summary>
+[CompileTime]
+public static class IMethodBaseBuilderExtensions
+{
+    /// <summary>
+    /// Adds a new parameter to the method based on a <see cref="ParameterDefinition"/>.
+    /// </summary>
+    /// <param name="methodBaseBuilder">The <see cref="IMethodBaseBuilder"/> to add the parameter to.</param>
+    /// <param name="parameterDefinition">The definition of the parameter to add.</param>
+    /// <returns>An <see cref="IParameterBuilder"/> for the new parameter.</returns>
+    public static IParameterBuilder AddParameter(
+        this IMethodBaseBuilder methodBaseBuilder,
+        ParameterDefinition parameterDefinition
+    )
+    {
+        return methodBaseBuilder.AddParameter(
+            parameterDefinition.Name,
+            parameterDefinition.Type,
+            parameterDefinition.RefKind,
+            parameterDefinition.DefaultValue
+        );
+    }
+
+    /// <summary>
+    /// Adds a range of new parameters to the method based on a collection of <see cref="ParameterDefinition"/> objects.
+    /// </summary>
+    /// <param name="methodBaseBuilder">The <see cref="IMethodBaseBuilder"/> to add the parameters to.</param>
+    /// <param name="parameterDefinitions">The definitions of the parameters to add.</param>
+    /// <returns>A collection of <see cref="IParameterBuilder"/> objects for the new parameters.</returns>
+    public static IEnumerable<IParameterBuilder> AddParameterRange(
+        this IMethodBaseBuilder methodBaseBuilder,
+        IEnumerable<ParameterDefinition> parameterDefinitions
+    )
+    {
+        List<IParameterBuilder> parameterBuilders = [];
+
+        foreach (var parameterDefinition in parameterDefinitions)
+        {
+            var builder = methodBaseBuilder.AddParameter(parameterDefinition);
+
+            parameterBuilders.Add(builder);
+        }
+
+        return parameterBuilders;
+    }
+}
